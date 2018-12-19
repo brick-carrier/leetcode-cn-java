93fc0485fff95f966dad953df6cde727ea027869
brick-carrier <tomkimjam@gmail.com>
2018-12-19 10:34:50 +0800

    删除lambda

--- src/Lambda/ILambda.java
+++
@@ -1,6 +0,0 @@
-package Lambda;
-
-public interface ILambda {
-
-    public boolean testBoolean(String num);
-}
--- src/Lambda/LambdaImpl.java
+++
@@ -1,9 +0,0 @@
-package Lambda;
-
-public class LambdaImpl implements ILambda{
-
-    @Override
-    public boolean testBoolean(String num) {
-        return num.equals("10");
-    }
-}
