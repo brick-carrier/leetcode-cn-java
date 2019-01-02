213028dedccc35fbd9ae4fad3f5f2debc74cc7fe
brick-carrier <tomkimjam@gmail.com>
2019-01-02 10:47:06 +0800

    添加100、Same Tree

--- src/LeetCode/Easy/SameTree.java
+++ src/LeetCode/Easy/SameTree.java
@@ -10,26 +10,28 @@ package LeetCode.Easy;
  Example 1:
 
  Input:     1         1
- / \       / \
- 2   3     2   3
+           / \       / \
+          2   3     2   3
 
  [1,2,3],   [1,2,3]
 
  Output: true
+
  Example 2:
 
  Input:     1         1
- /           \
- 2             2
+           /           \
+          2             2
 
  [1,2],     [1,null,2]
 
  Output: false
+
  Example 3:
 
  Input:     1         1
- / \       / \
- 2   1     1   2
+           / \       / \
+          2   1     1   2
 
  [1,2,1],   [1,1,2]
 
